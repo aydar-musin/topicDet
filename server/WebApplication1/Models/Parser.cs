f9b88a87ebf2de62c3fc08d59ba1bbf7fb66e2b9
Aydar <21pomni@gmail.com>
2015-08-05 23:19:12 +0300

    added server part

---
+++ server/WebApplication1/Models/Parser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.IO;
+
+namespace WebApplication1.Models
+{
+    public static class Parser
+    {
+        public static List<Photo> LoadPhotos()
+        {
+            string file_path = WebConfigurationManager.AppSettings["photos_file_path"];
+            var lines = File.ReadAllLines(file_path);
+
+            List<Photo> result = new List<Photo>();
+
+            foreach (var line in lines)
+            {
+                var args = line.Split('\t');
+
+                Photo photo = new Photo();
+                photo.Id = args[0];
+                photo.DateTime = DateTime.Parse(args[1]);
+                photo.Text = args[2];
+                photo.PhotoUrl = args[3];
+                result.Add(photo);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ server/WebApplication1/Models/Photo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Photo
+    {
+        public string Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string PhotoUrl { get; set; }
+        public string Text { get; set; }
+    }
+}
